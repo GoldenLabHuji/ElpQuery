68549b3f3deeba4852f659f276cd9a69433c7456
Shlomi Shitrit <akunh113@gmail.com>
2023-12-27 14:07:23 +0200

    Add string attribute and some docstrings

--- models/attributes.py
+++ models/attributes.py
@@ -8,11 +8,34 @@ Operator = "<" | ">" | "="
 class NumericAttribute(BaseModel):
     """
     class for numeric attributes
+
+    Attributes
+    ----------
+    value : float
+        the value of the attribute
+    operator : Operator
+        the operator to be used for the comparison
+    std : float
+        the standard deviation of the attribute
+
     """
 
     value: float
     operator: Operator
     std: float
+
+
+class StringAttribute(BaseModel):
+    """
+    class for string attributes
+
+    Attributes
+    ----------
+    value : str
+        the value of the attribute
+    """
+
+    value: str
 
 
 class NPhon(NumericAttribute):

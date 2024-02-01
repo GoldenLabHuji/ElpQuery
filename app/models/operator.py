5e5b2c71a27eb041c1cb7ee8d67924c88e52f773
Shlomi Shitrit <akunh113@gmail.com>
2024-02-01 17:21:26 +0200

    change operator to class enum

---
+++ app/models/operator.py
@@ -0,0 +1,15 @@
+"""
+Module for Operator model
+"""
+
+from enum import Enum
+
+
+class Operator(Enum):
+    """
+    class for the operators enum
+    """
+
+    GREATER = "Greater"
+    LOWER = "Lower"
+    EQUAL = "Equal"

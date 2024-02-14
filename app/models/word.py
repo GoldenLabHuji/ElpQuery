8570eaf2fe251253b49f93d89b8cae61e3ad572a
Shlomi Shitrit <akunh113@gmail.com>
2024-02-14 14:27:51 +0200

    Add string attribute Fixes #30

--- app/models/word.py
+++ app/models/word.py
@@ -1,14 +1,16 @@
 """
 Module of the word model
 """
+
 from typing import Optional
 from pydantic import BaseModel
-from app.models.attribute import NumericAttribute
+from app.models.attribute import NumericAttribute, StringAttribute
 
 
 class Word(BaseModel):
     """
     This is the word model
+
     Attributes:
     -----------
     age_of_aquisition: int
@@ -17,8 +19,14 @@ class Word(BaseModel):
          The number of phonemes in the main pronunciation
     n_syll: int
         The number of syllables in the main pronunciation
+    start_with: str
+        For query of words whos start with a specific string
+    sound_like: str
+        For query of words that sound like a specific string
     """
 
     age_of_aquisition: Optional[NumericAttribute] = None
     n_phon: Optional[NumericAttribute] = None
     n_syll: Optional[NumericAttribute] = None
+    start_with: Optional[StringAttribute] = None
+    sound_like: Optional[StringAttribute] = None
